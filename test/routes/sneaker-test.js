9de3b91036bda61ec6f9857117c378d042ecd942
WujiugeYin <ywjg_99@126.com>
2018-11-03 18:08:55 +0000

    Add sneakers endpoint tested

--- test/routes/sneaker-test.js
+++ test/routes/sneaker-test.js
@@ -29,4 +29,39 @@ describe('Sneaker', function (){
         });
     });
 	
+	describe('POST /sneaker', function () {
+        it('should return confirmation message and update db', function(done) {
+            let sneaker = {            
+		_id: 60000008,
+		brand: "Nike",
+		series: "Air Jordan 4 Retro",
+		name: "Eminem Carharrt",
+		color: "Retro Black/Metallic Sliver",
+		original_price: 10000,
+		article_number: "136863",
+		release_date:'2015-11-23',
+            };
+            chai.request(server)
+                .post('/sneaker')
+                .send(sneaker)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message').equal('Sneaker Info Successfully Added!' );
+                    done();
+                });
+        });
+        after(function  (done) {
+            chai.request(server)
+                .get('/sneaker')
+                .end(function(err, res) {
+                    let result = _.map(res.body, (sneaker) => {
+                        return { brand: sneaker.brand,
+                            article_number: sneaker.article_number };
+                    }  );
+					 expect(res.body.length).to.equal(8);
+                    expect(result).to.include( { brand: 'Nike', article_number: '136863' } );
+                    done();
+                });
+        });
+    });
 });
