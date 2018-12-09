106918e10bf29fb9df9825772fd85bf180888082
WujiugeYin <ywjg_99@126.com>
2018-12-09 14:41:43 +0000

    Free of lint errors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/ eslint no-unused-vars：“off” /
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //var cors = require('cors');
+
 
 const selling = require("./routes/selling");
 const account = require("./routes/account");
